269e2d8c55e1069adb37a9c26eeaf885dd24ced1
Javier <javier@javier.aurix.com>
2021-03-15 15:04:49 -0400

    vue axios implement cdn

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from models import Book, Author
 
 app = FastAPI()
+
+app.add_middleware(
+CORSMiddleware,
+allow_origins=["*"], # Allows all origins
+allow_credentials=True,
+allow_methods=["*"], # Allows all methods
+allow_headers=["*"], # Allows all headers
+)
 
 @app.get("/")
 async def root():
